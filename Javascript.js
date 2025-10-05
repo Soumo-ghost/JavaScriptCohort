57fb36e0c7594143e750b59f9486820e19639d0c
Soumo-ghost <chakrabortypremanshu@gmail.com>
2025-10-05 15:25:36 +0530

    ee

---
+++ Javascript.js
@@ -0,0 +1,23 @@
+console.log("Hi")
+//const a var type that can value can not be change
+const soumo=10
+console.log(soumo)
+//val or let act same ,val used in old js programing but for now alawys use let
+let a="soumosish"
+console.log(a);
+let b=10
+console.log(b);
+//we can also decle variable with out using any type but it is bad practice
+c="soumasish" 
+console.log(c);
+
+//Very importent
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log("1"+"2");
+
+
+
+
