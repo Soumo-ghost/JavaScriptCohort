378a4ad838d9f95297afff7745d02b1e3c187461
Soumo-ghost <chakrabortypremanshu@gmail.com>
2025-10-06 15:32:10 +0530

    summery

---
+++ summery/datatype.js
@@ -0,0 +1,13 @@
+//ptimitive
+//7 type string, Number, Boolean, null, undefined, symbol, Bigint
+const core=100
+const s=false
+const id=Symbol('123')
+const aid=Symbol('123')
+console.log(id===aid);
+const bigno=1234549978999
+
+
+
+//Referance type or non primitive
+//Array, Objects, Functions
