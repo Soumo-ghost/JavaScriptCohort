f51bf2098351e00f313edc844f0b2ad3c4f24c68
Soumo-ghost <chakrabortypremanshu@gmail.com>
2025-10-06 17:09:24 +0530

    date half

---
+++ dateinjs.js
@@ -0,0 +1,8 @@
+let mydate= new Date()
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+console.log(typeof mydate);
+let mycdate= new Date(2023,0,23);
+console.log(mycdate.toDateString());
+
