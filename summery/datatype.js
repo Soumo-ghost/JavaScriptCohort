4870fbe7f6cfeeb5559f8d07ff0ce9948994cb92
Soumo-ghost <chakrabortypremanshu@gmail.com>
2025-10-06 15:33:04 +0530

    summery

--- summery/datatype.js
+++ summery/datatype.js
@@ -5,9 +5,17 @@ const s=false
 const id=Symbol('123')
 const aid=Symbol('123')
 console.log(id===aid);
-const bigno=1234549978999
-
-
+const bigno=12345499789499n
 
 //Referance type or non primitive
 //Array, Objects, Functions
+const hero=["somo","soumasish","soumasiscgg"]
+let myobj={
+    name:"soumo",
+    age:22,
+}
+const myfunction= function(){
+    console.log("hi ");
+}
+console.log(typeof bigno);
+ 
