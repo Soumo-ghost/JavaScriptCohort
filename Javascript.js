79331b82465c71c6461e460c090ed472f24f0820
Soumo-ghost <chakrabortypremanshu@gmail.com>
2025-10-06 15:02:30 +0530

    dd

--- Javascript.js
+++ Javascript.js
@@ -17,6 +17,47 @@ console.log(1+"2");
 console.log("1"+2+2);
 console.log(1+2+"2");
 console.log("1"+"2");
+//dont use it (3+4*4\5)
+//for more info ECMA
+console.log(true); 
+//(+true) give 1
+console.log( +"");
+//assignment operator
+let n1, n2, n3
+n1=n2=n3=2+2//dont use like that use better 
+let gamecouter=100
+gamecouter++
+console.log(gamecouter);//learn pre and post precedent
+//learn increment operators documentation
+//--------------------##################-----------------
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+//==================
+console.log("2">1);//string vs int
+console.log("02">=1);
+
+
+console.log(null>0);
+console.log(null==1);
+console.log(null>=1);
+//Learm ECMA Documentation importent
+ console.log(undefined==1);//false
+ //comperison and equality check in js is different operation
+ 
+ //strict check 
+ console.log("2"===1);
+
+ //lec 8 complete cac
+
+
+
+
+
+
+
 
 
 
