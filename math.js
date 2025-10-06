e65ea783cb3ba56e88d60204aa814eaa539ec512
Soumo-ghost <chakrabortypremanshu@gmail.com>
2025-10-06 16:56:55 +0530

    Math

---
+++ math.js
@@ -0,0 +1,33 @@
+const score=20;  
+const scoreborad =new Number(1000)
+//will give special massage it is a number
+console.log(scoreborad);
+console.log(scoreborad.toString().length);
+console.log(scoreborad.toFixed(2));
+const othernumber=45.56;
+console.log(othernumber.toPrecision(2));//return a sting
+//will round off the valus 
+const hund=10000;
+console.log(hund.toLocaleString('en-IN'));
+//learn Number and there funtion for comprtitive programing
+//-----------Math------------
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.63));
+console.log(Math.ceil(4.1));//just inccre get next value
+console.log(Math.floor(5.6));
+console.log(Math.min(5 ,6 ,7,9));
+console.log(Math.random);//always give any nalur btween 0-1
+console.log((Math.random()*10 )+1);
+const min=10;
+const max=30;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+//to get atleats one value we add 1 also to get minimun value we add minimum
+
+
+
+
+
+
+
+
