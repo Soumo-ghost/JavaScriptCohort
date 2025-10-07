98b3d231a93a7862bc053979b2697a8282a9af93
Soumo-ghost <chakrabortypremanshu@gmail.com>
2025-10-07 14:42:37 +0530

    Date basic complete

--- dateinjs.js
+++ dateinjs.js
@@ -5,4 +5,25 @@ console.log(mydate.toLocaleString());
 console.log(typeof mydate);
 let mycdate= new Date(2023,0,23);
 console.log(mycdate.toDateString());
+let datee=new Date("01-14-2023");
+
+let timestamp= Date.now()
+console.log(timestamp);
+console.log(timestamp.getTime());
+console.log(Math.floor(Date.now()));
+
+let newdate= new datee();
+console.log(newdate);
+console.log(newdate.getMonth());
+console.log(newdate.getDay());
+
+//
+newdate.toLocaleString('default',{
+    weekday:"long",
+})
+
+
+
+
+
 
