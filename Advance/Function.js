8186c0e522da6faeeadf983af9368016d6af7cd1
Soumo-ghost <chakrabortypremanshu@gmail.com>
2025-10-09 23:08:11 +0530

    Function

--- Advance/Function.js
+++ Advance/Function.js
@@ -4,3 +4,27 @@ function sayMyName(){
 }
 sayMyName()//It called execution
 sayMyName//it is referance
+// function addtwoNum(num1, num2){//parameter are need
+
+
+//     console.log(num1+num2);
+// }
+//calling
+// addtwoNum(2,4);//argumnet are passed
+// //parameter and argument
+// const result=addtwoNum(3, 5);
+// console.log(result);
+function addtwoNumber(num1, num2){
+    let result=num1+num2;
+    return result
+}
+const result=addtwoNumber(3,5);
+// console.log(result);
+function login(username="sam"){//it now never is empty becuse it has a value if we give it externaly then privious value get repleced
+    if(username===undefined){//if(!username) can also be used good programing
+        console.log("plese enter user name");
+        return
+    }
+return `${username} jus tloged in`
+}
+console.log(login("Soumo"));//if no value is passed user is undifined
