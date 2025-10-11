8d642d47f117bbe0e27b55819995101d9f822d2e
Soumasish Chakraborty <96785880+Soumo-ghost@users.noreply.github.com>
2025-10-11 18:12:41 +0530

    swithch

--- Advance/control_flow.js
+++ Advance/control_flow.js
@@ -44,10 +44,42 @@ const debitCard = true
 const loggedInFromGoogle = false
 const loggedInFromEmail = true
 
-if (userLoggedIn && debitCard && 2==3) {
+if (userLoggedIn && debitCard && 2==3) 
+    {
     console.log("Allow to buy course");
     }
 
-    if (loggedInFromGoogle || loggedInFromEmail) {
+if (loggedInFromGoogle || loggedInFromEmail) 
+    {
         console.log("User logged in");
-        }
+    }
+    
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+            console.log("January");
+                    break;
+                        case "feb":
+                                console.log("feb");
+                                        break;
+                                            case "march":
+                                                    console.log("march");
+                                                            break;
+                                                                case "april":
+                                                                        console.log("april");
+                                                                                break;
+
+                                                                                    default:
+                                                                                            console.log("default case match");
+                                                                                                    break;
+                                                                                                    }
