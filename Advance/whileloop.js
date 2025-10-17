a50e155ada419b693244d19c5aabf143069131b2
Soumo-ghost <chakrabortypremanshu@gmail.com>
2025-10-17 16:54:02 +0530

    loops

---
+++ Advance/whileloop.js
@@ -0,0 +1,20 @@
+
+let i=0;
+while (i<12){
+console.log(i);
+i++;
+}
+let arro=0;
+let arr=["soi,mo","soumoo","soumokss"]
+while (arro < arr.length) {
+    console.log(`superhero is ${arr[arro]}`);
+    arro++
+}
+
+//do while
+//do while loop is special it atlest runs onece
+let score =1;
+do {
+console.log(score);
+score++;
+}while(score<=10);
