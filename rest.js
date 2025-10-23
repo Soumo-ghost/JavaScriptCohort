bc00381f3c805664ce4581de48e5cdc833ca5c83
Soumo-ghost <chakrabortypremanshu@gmail.com>
2025-10-23 22:05:57 +0530

    Timepass

---
+++ rest.js
@@ -0,0 +1,21 @@
+// var runningSum = function(nums) {
+//     const arr=[];
+//     for(var i=0;i<nums.length;i++){
+//         if(i==0){
+//             arr[i]=nums[i];
+//         }
+//         else{
+//             arr[i]=nums[i]+arr[i-1];
+//         }
+    
+//     }
+//     return arr
+// };
+const a=[[1,2],[5,6],[6,8]];
+const b=runningSum(a)
+console.log(b);
+// console.log(a.length);
+
+var maximumWealth = function(accounts) {
+    
+};
